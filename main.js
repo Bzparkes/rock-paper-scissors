7328350f603546b8db96a3e447bf149a5bf73120
Bzparkes <bzparkes@yahoo.com.au>
2022-03-29 20:02:43 +1000

    Tricky problem. Need more study to solve this without cheating

--- main.js
+++ main.js
@@ -1,2 +1,27 @@
-console.log('Hello World')
+// console.log('Hello World')
 
+function computerPlay(gameWord) {
+    return gameWord[Math.floor(Math.random()*gameWord.length)];
+}
+
+const gameWord = ['Rock', 'Paper', 'Scissors'];
+//console.log(computerPlay(gameWord));
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection.toLowerCase() === computerSelection) {
+    return 'It\'s a draw!';
+}   else if(computerSelection === 'Scissors') {
+    return 'You win!'; 
+}   else {
+    return 'You lose!'
+}  
+}
+
+const playerSelection = 'Rock';
+const computerSelection = computerPlay(gameWord);
+//console.log(playRound(playerSelection, computerSelection));
+
+function game(playRound) {
+   if 
+ 
+console.log(game)
