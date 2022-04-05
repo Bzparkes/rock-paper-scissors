77b6cbbf76abe235599931bc36996588714fc58d
Bzparkes <bzparkes@yahoo.com.au>
2022-04-05 18:54:04 +1000

    Restarted this project. Got the random word function working

--- main.js
+++ main.js
@@ -1,27 +1,9 @@
-// console.log('Hello World')
+//console.log('Hello World')
 
-function computerPlay(gameWord) {
-    return gameWord[Math.floor(Math.random()*gameWord.length)];
-}
-
-const gameWord = ['Rock', 'Paper', 'Scissors'];
-//console.log(computerPlay(gameWord));
-
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection.toLowerCase() === computerSelection) {
-    return 'It\'s a draw!';
-}   else if(computerSelection === 'Scissors') {
-    return 'You win!'; 
-}   else {
-    return 'You lose!'
-}  
-}
-
-const playerSelection = 'Rock';
-const computerSelection = computerPlay(gameWord);
-//console.log(playRound(playerSelection, computerSelection));
-
-function game(playRound) {
-   if 
- 
-console.log(game)
+function computerPlay() {
+    const answers = ['Rock', 'Paper', 'Scissors'];
+    const random =  answers[Math.floor(Math.random()*answers.length)];
+    alert(random);
+    }
+    
+//computerPlay()
