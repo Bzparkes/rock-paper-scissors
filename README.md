0d8f8f3b28b7933ccf74d506ef12a9f965e9eea0
Bzparkes <bzparkes@yahoo.com.au>
2022-04-19 18:53:17 +1000

    Finished the RPS project

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 First js project, given by The Odin Project.
 
 
+Completed the project. Couldn't have done this on my own, as the instruction in TOP
+don't actually make any sense.
--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 const choices = ["rock", "paper", "scissors"];
+const winners = [];
 
 function game() {
-    for(let i = 0; i <= 5; i++) {
-        playRound();
+    for(let i = 1; i <= 5; i++) {
+        playRound(i);
     }  
+    document.querySelector("button").textContent = "Play again";
+    logWins();
 }
 
-function playRound() {
+function playRound(round) {
    const playerSelection = playerChoice();
    const computerSelection = computerChoice();
    const winner = checkWinner(playerSelection,computerSelection);
-   console.log(winner);
+   winners.push(winner);
+   logRound(playerSelection,computerSelection,winner,round);
 }
 
 function playerChoice() {
@@ -53,13 +57,32 @@ function checkWinner(choiceP, choiceC) {
         (choiceP === "paper" && choiceC === "rock") || 
         (choiceP === "scissors" && choiceC === "paper")
     ) {
-      return "Player wins!";
+      return "Player";
     } else{
-        return "Computer wins!";
+        return "Computer";
     }
 }
 
-game();
+function logWins() {
+    let playerWins = winners.filter((item) => item == "Player").length;
+    let computerWins = winners.filter((item) => item == "Computer").length;
+    let ties = winners.filter((item) => item == "Tie").length;
+    console.log("Results:")
+    console.log("Player Wins:",playerWins);
+    console.log("Computer Wins:",computerWins);
+    console.log("Ties:",ties);
+}
+
+function logRound(playerChoice,computerChoice,winner,round) {
+    console.log("Round", round)
+    console.log("Player Chose:",playerChoice);
+    console.log("Computer Chose:",computerChoice);
+    console.log(winner, "Won the round");
+    console.log('------------------------------------------------');
+}
+
+
+//game();
 
 
 
